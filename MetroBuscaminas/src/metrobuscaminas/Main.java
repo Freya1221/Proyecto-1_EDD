67620b2ba34bbfca716d627fdd6978a2224eab40
Freya1221 <Freyablanca10013@gmail.com>
2025-03-05 11:18:01 -0400

    Interfaz de inicio

--- MetroBuscaminas/src/metrobuscaminas/Main.java
+++ MetroBuscaminas/src/metrobuscaminas/Main.java
@@ -14,7 +14,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Inicio");
+        Home home = new Home();
+        home.setVisible(true);
     }
     
 }
