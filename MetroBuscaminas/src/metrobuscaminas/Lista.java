0cf42223d3bba1200e1fb69f08bf05d37e12261e
Jasikob <schneiderjesus02@gmail.com>
2025-03-08 20:04:59 -0400

    JavaDoc: Lista

--- MetroBuscaminas/src/metrobuscaminas/Lista.java
+++ MetroBuscaminas/src/metrobuscaminas/Lista.java
@@ -5,12 +5,18 @@
 package metrobuscaminas;
 
 /**
- *
- * @author adcd_
+ * Clase que representa una lista enlazada simple utilizada en el juego MetroBuscaminas.
+ * La lista almacena enteros y se usa para gestionar adyacencias en el grafo del tablero.
+ * 
+ * @author Freya Blanca, Jesús Schneider
  */
 public class Lista {
     private Nodo inicio;
     
+    /**
+     * Constructor de la lista enlazada.
+     * Inicializa la lista vacía.
+     */
     public Lista() {
         this.inicio = null;
     }
@@ -23,11 +29,22 @@ public class Lista {
         this.inicio = inicio;
     }
     
+    /**
+     * Inserta un nuevo nodo al inicio de la lista.
+     * 
+     * @param x Valor entero a insertar en la lista.
+     */
     public void insertar(int x) {
         Nodo nuevo = new Nodo(x, this.getInicio());
         this.setInicio(nuevo);
     }
     
+    /**
+     * Elimina un nodo con un valor específico de la lista.
+     * Si el nodo no existe, la lista permanece igual.
+     * 
+     * @param x Valor del nodo a eliminar.
+     */
     public void eliminar(int x) {
         if (this.getInicio() == null) return;
         if (this.getInicio().getClave() == x) {
@@ -44,6 +61,12 @@ public class Lista {
         }
     }
     
+    /**
+     * Busca un valor en la lista.
+     * 
+     * @param x Valor a buscar.
+     * @return true si el valor está en la lista, false en caso contrario.
+     */
     public boolean busqueda(int x) {
         Nodo actual = this.getInicio();
         while (actual != null) {
@@ -56,6 +79,11 @@ public class Lista {
     }
  
     
+    /**
+     * Cuenta el número de elementos en la lista.
+     * 
+     * @return Cantidad de nodos en la lista.
+     */
     public int numElementos() {
         int count = 0;
         Nodo actual = this.getInicio();
@@ -66,6 +94,9 @@ public class Lista {
         return count;
     }
     
+    /**
+     * Imprime los valores de la lista en la consola.
+     */
     public void imprimir() {
         Nodo actual = this.getInicio();
         while(actual != null) {
